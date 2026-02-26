fa9c4433009c26cdc4df1726d83d3c22615ec4f4
Henry, Reinhard Martin <henry.reinhard.martin@gmail.com>
2026-02-26 11:11:47 +0100

    Weißer Bildschirm 2.
    Weißer Bildschirm 2.

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,8 @@
 
-const CACHE_NAME = 'health-tracker-v6';
+const CACHE_NAME = 'health-tracker-v5';
 const ASSETS = [
-  '/gesundheits-tracker-web-1/',
-  '/gesundheits-tracker-web-1/index.html',
-  '/gesundheits-tracker-web-1/manifest.json',
+  'index.html',
+  'manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
